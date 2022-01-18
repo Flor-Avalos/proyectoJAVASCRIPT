54ceb277d1e26a8c1051fec3ab9032ac37ea689d
Florencia Avalosclear <2021flor@gmail.com>
2022-01-18 17:44:19 -0300

    muestro en html las opciones

--- script.js
+++ script.js
@@ -9,25 +9,46 @@ class Producto {
 
 let productos= [];
 
-productos.push(new Producto(1, 'alfajor', 100))
-productos.push(new Producto(2, 'bombones', 120))
-productos.push( new Producto(3, 'tableta', 340))
+productos.push(new Producto(1, 'Alfajor de DDL', 100))
+productos.push(new Producto(2, 'Alfajor de Frambueza', 120))
+productos.push( new Producto(3, 'Alfajor de Frutos del bosque', 340))
+productos.push(new Producto(4, 'Alfajor de Calafate', 100))
+productos.push(new Producto(5, 'Bombones rellenos', 120))
+productos.push( new Producto(6, 'Chocolate en rama', 340))
 
 console.log(productos);
 
-const pedirDato= () =>{
-    const datoUsuario = parseInt(prompt('ingrese el id del producto'));
+const pedirDato= datoUsuario =>{
+   
     const productoEncontrado= productos.find(producto => producto.id === datoUsuario);
-    console.log(productoEncontrado);
+
+     if (productoEncontrado != undefined){
+    generarHtml(productoEncontrado)}
+    else{ console.log('No existe producto')}
+
+    
 }
-// pedirDato()
+
+//asocio el evento al submit y traigo el valor ingresado en el formulario
 
 const formulario= document.getElementById('formulario');
 formulario.addEventListener( 'submit',e => {
     e.preventDefault();
   const id= parseInt(formulario.productoId.value);
-  console.log(id);  
+  pedirDato(id);  
+  formulario.reset();
 } )
+
+// muestro el priducto en html, creo una UL con cada dato ingresado
+const generarHtml= producto =>{
+    const lista= document.getElementById('lista');
+    lista.innerHTML += `<li>${producto.nombre} - $${producto.precio}</li>`
+
+    // si agrego += con cada click acumula la eleccion del cliente
+    // con solo = reemplaza en cada eleccion
+}
+
+
 
 
 
